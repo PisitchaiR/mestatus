6e59b6334ed0bd9f8c2c7f9937cb2c154719c437
PisitchaiR <73678503+PisitchaiR@users.noreply.github.com>
2022-10-31 09:45:26 +0700

    resolve css

--- src/App.svelte
+++ src/App.svelte
@@ -2,25 +2,27 @@
   import { InstagramIcon, FacebookIcon, GithubIcon } from "svelte-feather-icons";
 </script>
 
-<main class="bg-primary w-full h-full flex items-center justify-center font-kanit">
-  <div class="rounded-md border shadow-flate p-20 lg:p-32 text-4xl lg:text-7xl flex flex-col justify-center items-center relative duration-1000">
+<main class="bg-primary w-screen h-screen flex items-center justify-center font-kanit px-5 md:px-16">
+  <div class="rounded-lg border shadow-flate w-full p-20 lg:w-2/3 lg:py-32 text-4xl  md:text-5xl flex flex-col justify-center items-center relative duration-1000">
     <div class="flex gap-x-5 absolute top-5 left-5">
-      <div class="w-6 h-6 bg-red-600 hover:bg-red-600/60 rounded-full" />
-      <div class="w-6 h-6 bg-yellow-400 hover:bg-yellow-400/60 rounded-full" />
-      <div class="w-6 h-6 bg-green-600 hover:bg-green-600/60 rounded-full" />
+      <div class="w-4 md:w-6 h-4 md:h-6 bg-red-600 hover:bg-red-600/60 rounded-full" />
+      <div class="w-4 md:w-6 h-4 md:h-6 bg-yellow-400 hover:bg-yellow-400/60 rounded-full" />
+      <div class="w-4 md:w-6 h-4 md:h-6 bg-green-600 hover:bg-green-600/60 rounded-full" />
     </div>
-    <p>Pisitchai Thun</p>
-    <p class="flex gap-x-10 mt-16">
-      <a href="https://www.instagram.com/pisitchai_thun/">
-        <InstagramIcon size="50" class="rounded-xl bg-primary shadow-flate p-2 w-20 h-20 hover:shadow-pressed" />
-      </a>
-      <a href="https://www.facebook.com/mctplo.admplo/">
-        <FacebookIcon size="50" class="rounded-xl bg-primary shadow-flate p-2 w-20 h-20 hover:shadow-pressed" />
-      </a>
+    <div class="">
+      <p class="w-full text-center">Pisitchai Thun</p>
+      <p class="flex gap-x-10 w-full justify-center mt-10">
+        <a href="https://www.instagram.com/pisitchai_thun/">
+          <InstagramIcon size="50" class="rounded-xl bg-primary shadow-flate p-2 w-16 lg:w-20 h-16 lg:h-20 hover:shadow-pressed" />
+        </a>
+        <a href="https://www.facebook.com/mctplo.admplo/">
+          <FacebookIcon size="50" class="rounded-xl bg-primary shadow-flate p-2 w-16 lg:w-20 h-16 lg:h-20 hover:shadow-pressed" />
+        </a>
 
-      <a href="https://github.com/PisitchaiR">
-        <GithubIcon size="50" class="rounded-xl bg-primary shadow-flate p-2 w-20 h-20 hover:shadow-pressed" />
-      </a>
-    </p>
+        <a href="https://github.com/PisitchaiR">
+          <GithubIcon size="50" class="rounded-xl bg-primary shadow-flate p-2 w-16 lg:w-20 h-16 lg:h-20 hover:shadow-pressed" />
+        </a>
+      </p>
+    </div>
   </div>
 </main>
